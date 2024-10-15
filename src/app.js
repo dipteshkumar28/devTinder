79dba110b02cc1cd479688b67a9d7c794b574048
Diptesh Kumar <dipteshsongara2004@gmail.com>
2024-10-15 23:30:02 +0530

    Exploring get, post,delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello world again!!");
-});
+// app.use( "/",(req, res) => {
+//   res.send("Hello world again!!");
+// });
 
 app.use("/diptesh", (req, res) => {
-  res.send("Diptesh Kumar");
+  res.send("Diptesh Kumar ");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello world");
+//this will only handle get api(s)
+app.get("/user",(req,res)=>{
+   res.send({firstname:"Diptesh",lastname:"kumar"});
 });
 
-app.listen(3000);
+app.post("/user",(req,res)=>{
+   res.send({College:"IIT Roorkee", Branch:"Metallurgical And Materials Engineering"});
+});
+// app.use("/diptesh/kumar", (req, res) => {
+//   res.send("Diptesh songara");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello world");
+// });
+// app.use("/", (req, res) => {
+//   res.send("Hello world again!!");
+// });
+app.listen(7777);
