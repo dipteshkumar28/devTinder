703922b2f8eae2a739880909f665fa046755ec38
Diptesh Kumar <dipteshsongara2004@gmail.com>
2024-10-16 10:01:17 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -11,11 +11,38 @@ const app = express();
 // });
 
 //this will only handle get api(s)
-app.get("/user/:userId/:name",(req,res)=>{
-   console.log(req.params); 
-   res.send({firstname:"Diptesh",lastname:"kumar"});
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "Diptesh", lastname: "kumar" });
 });
 
+app.use(
+  "/user",[
+  (req, res, next) => {
+    console.log("router handler 1");
+    next();
+   //  res.send("Route Handler 1");
+  },
+  (req, res, next) => {
+    console.log("router handler 2");
+    next();
+   //  res.send("Route Handler 2");
+  }],
+  (req, res, next) => {
+    console.log("router handler 3");
+    next();
+   //  res.send("Route Handler 3");
+  },
+  (req, res, next) => {
+    console.log("router handler 4");
+    //  res.send("Route Handler 4");
+    next();
+  },
+  (req, res,next) => {
+   console.log("router handler 5");
+    res.send("Route Handler 5");
+  }
+);
 // app.post("/user",(req,res)=>{
 //    res.send({College:"IIT Roorkee", Branch:"Metallurgical And Materials Engineering"});
 // });
